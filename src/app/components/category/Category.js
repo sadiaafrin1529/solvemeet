7822665723a2f0b124cf3b45fee071a5cd215e40
unknown <sadiaafrin1529@gmail.com>
2024-10-07 20:05:29 +0600

    aolveMeet

--- src/app/components/category/Category.js
+++ src/app/components/category/Category.js
@@ -4,47 +4,48 @@ import getCategory from "@/lib/getCategory";
 import Image from "next/image";
 import React from "react";
 import Slider from "react-slick";
-import { useEffect } from "react/cjs/react.production.min";
 
-export default function Category() {
- //demo.webcodecare.com/api/categoriesApi
- https: useEffect;
+export default async function Category() {
+  // Fetch categories
+  //https://demo.webcodecare.com/api/categoriesApi
+  // Log the categories in the server console
+
   // Slider settings
-  
- var settings = {
-   dots: true,
-   infinite: false,
-   speed: 500,
-   slidesToShow: 4,
-   slidesToScroll: 4,
-   initialSlide: 0,
-   responsive: [
-     {
-       breakpoint: 1024,
-       settings: {
-         slidesToShow: 3,
-         slidesToScroll: 3,
-         infinite: true,
-         dots: true,
-       },
-     },
-     {
-       breakpoint: 600,
-       settings: {
-         slidesToShow: 2,
-         slidesToScroll: 2,
-         initialSlide: 2,
-       },
-     },
-     {
-       breakpoint: 480,
-       settings: {
-         slidesToShow: 1,
-         slidesToScroll: 1,
-       },
-     },
-   ],
- };
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <div className="mt-10 container mx-auto">
       <Slider {...settings}>
