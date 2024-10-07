be93a096b1060dda1b3df7c8e846d0c16890f51c
rubelrocks <md.rubel007@hotmail.com>
2024-10-07 19:56:40 +0600

    image pbm and category code change

--- src/app/components/category/Category.js
+++ src/app/components/category/Category.js
@@ -1,75 +1,70 @@
-"use client"
-
-import getCategory from "@/lib/getCategory";
-import Image from "next/image";
-import React from "react";
+"use client";
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import Slider from "react-slick";
-import { useEffect } from "react/cjs/react.production.min";
 
 export default function Category() {
- //demo.webcodecare.com/api/categoriesApi
- https: useEffect;
-  // Slider settings
-  
- var settings = {
-   dots: true,
-   infinite: false,
-   speed: 500,
-   slidesToShow: 4,
-   slidesToScroll: 4,
-   initialSlide: 0,
-   responsive: [
-     {
-       breakpoint: 1024,
-       settings: {
-         slidesToShow: 3,
-         slidesToScroll: 3,
-         infinite: true,
-         dots: true,
-       },
-     },
-     {
-       breakpoint: 600,
-       settings: {
-         slidesToShow: 2,
-         slidesToScroll: 2,
-         initialSlide: 2,
-       },
-     },
-     {
-       breakpoint: 480,
-       settings: {
-         slidesToShow: 1,
-         slidesToScroll: 1,
-       },
-     },
-   ],
- };
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`https://demo.webcodecare.com/api/categoriesApi`);
+        const result = await res.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+console.log(data?.data)
   return (
     <div className="mt-10 container mx-auto">
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
+        <Slider {...settings}>
+          {data?.data?.map((item) => (
+            <div key={item?.id}>
+              <h3 className="text-2xl">{item?.name}</h3>
+              <Image width={70} height={70} src={item?.image} />
+            </div>
+          ))}
+        </Slider>
     </div>
   );
 }
-
-
-
